bec423751db3ccebf282ed33fe344208aad4ec0d
Nick Tolley <nic.m.tolley@gmail.com>
2023-09-18 10:27:25 -0700

    beta tuning

--- code/beta_tuning/generate_SBI_sims_beta_tuning.py
+++ code/beta_tuning/generate_SBI_sims_beta_tuning.py
@@ -3,13 +3,21 @@ sys.path.append('../')
 import os
 import dill
 from utils import (linear_scale_forward, log_scale_forward, start_cluster,
-                   run_hnn_sim, beta_tuning_param_function, UniformPrior)
+                   run_hnn_sim, beta_tuning_param_function, UniformPrior,
+                   PriorBetaFiltered)
 from hnn_core import calcium_model, pick_connection
+from neurodsp.spectral import compute_spectrum, trim_spectrum
+from torch import optim
+from tqdm import tqdm
+import joblib
 import numpy as np
 
 nsbi_sims = 10_000
 tstop = 1000
+num_prior_fits = 4
+relative_beta_threshold = 0.3
 dt = 0.05
+fs = 1000 / dt
 
 net = calcium_model()
 
@@ -28,7 +36,6 @@ theta_extra = {'EI_connections': EI_connections, 'EE_connections': EE_connection
                'lamtha': 4.0}
    
 save_path = '/expanse/lustre/scratch/ntolley/temp_project/beta_tuning'
-# save_path = '../../data/beta_tuning'
 save_suffix = 'sbi'
 
 prior_dict = {'EI_gscale': {'bounds': (-2, 2), 'rescale_function': log_scale_forward},
@@ -42,7 +49,6 @@ prior_dict = {'EI_gscale': {'bounds': (-2, 2), 'rescale_function': log_scale_for
               'L2e_distal': {'bounds': (-4, 0), 'rescale_function': log_scale_forward},
               'L2i_distal': {'bounds': (-4, 0), 'rescale_function': log_scale_forward},
               'L5e_distal': {'bounds': (-4, 0), 'rescale_function': log_scale_forward},
-              'L5i_distal': {'bounds': (-4, 0), 'rescale_function': log_scale_forward},
               'L2e_proximal': {'bounds': (-4, 0), 'rescale_function': log_scale_forward},
               'L2i_proximal': {'bounds': (-4, 0), 'rescale_function': log_scale_forward},
               'L5e_proximal': {'bounds': (-4, 0), 'rescale_function': log_scale_forward},
@@ -52,7 +58,9 @@ prior_dict = {'EI_gscale': {'bounds': (-2, 2), 'rescale_function': log_scale_for
 with open(f'{save_path}/sbi_sims/prior_dict.pkl', 'wb') as f:
     dill.dump(prior_dict, f)
 
-sim_metadata = {'nsbi_sims': nsbi_sims, 'tstop': tstop, 'dt': dt, 'gid_ranges': net.gid_ranges, 'theta_extra': theta_extra}
+sim_metadata = {'nsbi_sims': nsbi_sims, 'tstop': tstop, 'dt': dt, 'gid_ranges': net.gid_ranges,
+                'num_prior_fits': num_prior_fits, 'relative_beta_threshold': relative_beta_threshold,
+                'theta_extra': theta_extra}
 with open(f'{save_path}/sbi_sims/sim_metadata.pkl', 'wb') as f:
     dill.dump(sim_metadata, f)
 
@@ -61,8 +69,46 @@ theta_samples = prior.sample((nsbi_sims,))
 
 start_cluster() # reserve resources for HNN simulations
 
-run_hnn_sim(net=net, param_function=beta_tuning_param_function, prior_dict=prior_dict,
-            theta_samples=theta_samples, tstop=tstop, save_path=save_path, save_suffix=save_suffix,
-            theta_extra=theta_extra)
+for flow_idx in range(num_prior_fits):
+    if flow_idx == 0:
+        prior = UniformPrior(parameters=list(prior_dict.keys()))
+    else:
+        prior = prior_filtered
+        
+    theta_samples = prior.sample((nsbi_sims,))
+    
+    save_suffix = f'sbi_{flow_idx}'
+    
+    run_hnn_sim(net=net, param_function=beta_tuning_param_function, prior_dict=prior_dict,
+                theta_samples=theta_samples, tstop=tstop, save_path=save_path, save_suffix=save_suffix,
+                theta_extra=theta_extra)
+    
+    dpl_filter = np.load(f'{save_path}/sbi_sims/dpl_sbi_{flow_idx}.npy')
+    theta_filter = np.load(f'{save_path}/sbi_sims/theta_sbi_{flow_idx}.npy')
+    
+    times = np.linspace(0, tstop / 1000, dpl_filter.shape[1])
+    times_mask = times > 0.2 # burn in time
+    
+    freqs, powers = compute_spectrum(dpl_filter[:, times_mask], fs, method='welch', avg_type='median')
+    freqs, powers = trim_spectrum(freqs, powers, [0, 100])
+    
+    beta_mask = (freqs > 13) & (freqs < 30)
+    beta_power = powers[:, beta_mask].sum(axis=1)
+    relative_beta = beta_power / powers.sum(axis=1)
+    
+    relative_beta_mask = relative_beta > relative_beta_threshold
+    
+    prior_filtered = PriorBetaFiltered(parameters=list(prior_dict.keys()))
+    optimizer = optim.Adam(prior_filtered.flow.parameters())
 
-os.system('scancel -u ntolley')
+    num_iter = 5000
+    for i in tqdm(range(num_iter)):
+        optimizer.zero_grad()
+        loss = -prior_filtered.flow.log_prob(inputs=theta_filter).mean()
+        loss.backward()
+        optimizer.step()
+    state_dict = prior_filtered.flow.state_dict()
+    joblib.dump(state_dict, f'{save_path}/flows/prior_filtered_flow_{flow_idx}.pkl')
+
+
+# os.system('scancel -u ntolley')
--- code/utils.py
+++ code/utils.py
@@ -18,6 +18,13 @@ import scipy
 from scipy.signal import periodogram, welch
 from sklearn.linear_model import LinearRegression
 from fooof import FOOOF
+from torch import optim
+
+from nflows.flows.base import Flow
+from nflows.distributions.normal import StandardNormal
+from nflows.transforms.base import CompositeTransform
+from nflows.transforms.autoregressive import MaskedAffineAutoregressiveTransform
+from nflows.transforms.permutations import ReversePermutation
 
 from hnn_core import jones_2009_model, simulate_dipole, pick_connection
 from hnn_core.params import _short_name
@@ -854,3 +861,100 @@ def get_posterior_predictive_check(x_val, x_cond, n_samples=10):
         dist_list.append(dist)
     dist_array = np.array(dist_list)
     return dist_array
+
+class PriorBetaFiltered():
+    """Class for creating a prior distribution from
+       heuristically filtered simulations"""
+    
+    def __init__(self, parameters):
+        self.parameters = parameters
+        nparams = len(parameters)
+        self.flow = self.__flow_init(nparams)
+        self.acc_rate = 0.0
+
+    def __flow_init(self, nparams):
+        num_layers = 5
+        base_dist = StandardNormal(shape=[nparams])
+        transforms = []
+        for _ in range(num_layers):
+            transforms.append(ReversePermutation(features=nparams))
+            transforms.append(MaskedAffineAutoregressiveTransform(features=nparams,
+                                hidden_features=50,
+                                context_features=None,
+                                num_blocks=2,
+                                use_residual_blocks=False,
+                                random_mask=False,
+                                activation=torch.tanh,
+                                dropout_probability=0.0,
+                                use_batch_norm=True))
+        transform = CompositeTransform(transforms)
+        return Flow(transform, base_dist)
+
+    def sample(self, sample_shape, return_acc_rate=False):
+        base_prior = UniformPrior(self.parameters)        
+        nsamples = sample_shape[0]
+        nsamples_keep = 0
+        samples_keep = torch.tensor([])
+        total_samples = 0
+        while nsamples_keep < nsamples:
+            samples = self.flow.sample(nsamples).detach()
+            total_samples = total_samples + len(samples)
+            indices = list()
+            for idx in range(len(samples)):
+                try:
+                    if base_prior.log_prob(samples[idx]) == 0:
+                        indices.append(idx)
+                except ValueError:
+                    pass
+                
+            samples_keep = torch.cat([samples_keep, samples[indices]])
+            nsamples_keep = len(samples_keep)
+        acc_rate = torch.tensor(nsamples_keep / total_samples)
+        samples = samples_keep[:nsamples]   
+        if return_acc_rate:
+            return samples, acc_rate
+        else:
+            return samples     
+
+    def log_prob(self, theta):
+        if self.acc_rate == 0.0:
+            # get the acceptance rate right away
+            _, acc_rate = self.sample((10_000,), return_acc_rate=True)            
+        return self.flow.log_prob(theta).detach() - torch.log(acc_rate)
+    
+class Flow_base(Flow):
+    def __init__(self, batch_theta, batch_x, embedding_net, n_layers=5,
+                 z_score_theta=True, z_score_x=True, device='cpu'):
+
+        # instantiate the flow
+        flow = build_nsf(batch_x=batch_theta,
+                         batch_y=batch_x,
+                         z_score_x=z_score_theta,
+                         z_score_y=z_score_x,
+                         embedding_net=embedding_net).to(device)
+
+        super().__init__(flow._transform, 
+                         flow._distribution, 
+                         flow._embedding_net)
+
+    def save_state(self, filename):
+        state_dict = {}
+        state_dict['flow'] = self.state_dict()
+        torch.save(state_dict, filename)
+
+    def load_state(self, filename):
+        state_dict = torch.load(filename, map_location=device)
+        self.load_state_dict(state_dict['flow'])
+
+def build_flow(batch_theta,
+               batch_x,
+               embedding_net,
+               **kwargs):
+
+    flow = Flow_base(batch_theta, 
+                     batch_x, 
+                     embedding_net,
+                     device,
+                     **kwargs).to(device)
+
+    return flow
